f58a91aef5c82d0c7a552c7723d00e156e432177
VoidAngelo <silferod@gmail.com>
2015-12-02 22:10:22 +0300

    C++ длинное число.

---
+++ sources/SDP/cppMenu/longints.h
@@ -0,0 +1,33 @@
+#ifndef CPPMENU
+#define CPPMENU
+
+class longints {
+    public:
+
+
+        void calc_longint(long scanned_num)
+        {
+            num=scanned_num;
+            counted=0;
+            while(num!=0)
+            {
+                temp=num%10;
+                num=num/10;
+                counted=counted+temp;
+            }
+        }
+
+        long return_longint()
+        {
+            return counted;
+        }
+
+    private:
+    protected:
+        long num;
+        int counted;
+        long temp;
+};
+
+#endif // CPPMENU
+
